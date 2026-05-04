e14bb5d62b88a0085bb115cccb6d8fc33f2a3be0
Sunro1994 <gwangjulsr@gmail.com>
2026-05-05 03:53:33 +0900

    chore(worklog): module-worklog 모듈 초기 셋업

---
+++ module-worklog/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.5.14"))
+    implementation(project(":module-shared"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.mysql:mysql-connector-j")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("com.h2database:h2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
---
+++ module-worklog/src/test/kotlin/com/atomiccv/worklog/WorklogTestApplication.kt
@@ -0,0 +1,8 @@
+package com.atomiccv.worklog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootApplication
+@ActiveProfiles("test")
+class WorklogTestApplication
