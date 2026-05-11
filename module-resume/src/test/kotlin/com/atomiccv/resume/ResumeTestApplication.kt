cd4b8dd0797ded7bbad382bf846f2d3d09bfafb2
goldrex123 <goldrex123@naver.com>
2026-05-11 14:56:11 +0900

    chore(resume): validation 의존성·테스트 앱 추가

---
+++ module-resume/src/test/kotlin/com/atomiccv/resume/ResumeTestApplication.kt
@@ -0,0 +1,8 @@
+package com.atomiccv.resume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootApplication
+@ActiveProfiles("test")
+class ResumeTestApplication
